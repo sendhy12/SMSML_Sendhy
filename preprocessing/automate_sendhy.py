7f0555c8cbfe9c44c58a49208c2b33c7bcfdc1cc
sendhy12 <sendhymaula@gmail.com>
2025-06-14 15:58:26 +0700

    Fix: add automate_sendhy.py for preprocessing workflow

--- preprocessing/automate_sendhy.py
+++ preprocessing/automate_sendhy.py
@@ -100,8 +100,8 @@ if __name__ == "__main__":
     preprocessor = DataPreprocessor()
     
     # Sesuaikan dengan dataset Anda
-    raw_data_path = "../dataset_raw/your_dataset.csv"
-    target_column = "target_column_name"
+    raw_data_path = "Eksperimen_SML_Sendhy\heart.csv"
+    target_column = "target"
     output_directory = "./dataset_preprocessing"
     
     # Run preprocessing
